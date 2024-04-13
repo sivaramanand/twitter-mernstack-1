edd17dcf87e93c9cdd5e20a5198ea7c143a63b68
Sivaram <notifysiva@gmail.com>
2024-04-13 21:22:06 +0530

    commit 13-04

--- frontend/twitterclone/src/Components/Login.js
+++ frontend/twitterclone/src/Components/Login.js
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const loginSignupHandler = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="flex items-center">
+      <div className="flex items-center justify-evenly w-[80%]">
         <div>
           <img
             className="ml-5"
             width={"300px"}
-            src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?w=740&t=st=1711866115~exp=1711866715~hmac=45b4b3f9990872bf923087ad74e0b87f1a8f55acbd8c9dafc480c9208eeadebb"
+            src="https://www.edigitalagency.com.au/wp-content/uploads/new-Twitter-logo-x-black-png-1200x1227.png"
+            alt="twitter-logo"
           />
         </div>
         <div>
-          <div className="font-bold text-5xl mb-4">HAPPENING NOW</div>
-          <from className="flex flex-col w-[50%]">
-            <input type="text" placeholder="Name"  className=""/>
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-          </from>
+          <div className="my-5">
+            <h1 className="font-bold text-6xl">Happening now.</h1>
+          </div>
+          <h1 className="mt-4 mb-2 text-2xl font-bold">
+            {isLogin ? "Log In" : "Sign Up"}
+          </h1>
+          <form className="flex flex-col w-[55%]">
+            {!isLogin && (
+              <div>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold w-full" 
+                />
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold w-full" 
+                />
+              </div>
+            )}
+            <input
+              type="email"
+              placeholder="Email"
+              className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
+            />
+            <button className="bg-[#1D9BF0] border-none py-2 my-4 rounded-full text-lg text-white">
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+            <h1>
+              {isLogin ? "Do not have an account?" : "Already have an account?"}
+              <span
+                onClick={loginSignupHandler}
+                className="font-bold text-blue-600 cursor-pointer"
+              >
+                {isLogin ? "Sign Up" : "Log In"}
+              </span>
+            </h1>
+          </form>
         </div>
       </div>
     </div>
